f3ab8b8da1f0ac361eee71e124073c6787ea954f
YoooTooo <son.toyo.mcmxc@gmail.com>
2020-06-24 21:21:39 +0000

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200618145208) do
+ActiveRecord::Schema.define(version: 20200623090819) do
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'stringra' for column 'password_digest'
